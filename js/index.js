2766ea895d49a4336e03241f7dbd4c6b2d637252
juscelino1982 <102321197+juscelino1982@users.noreply.github.com>
2023-03-29 20:46:06 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    $(".bntCadastrar").click(function () {
+        $(".cadastrar").fadeIn();
+        $(".login").fadeOut();
+
+    });
+
+    $(".bntEntrar").click(function () {
+        $(".login").fadeIn();
+        $(".cadastrar").fadeOut();
+
+    });
+
+});
